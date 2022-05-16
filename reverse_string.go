dac1afac2fe250bdfc861b0242107ee7f841efe9
sansej5000 <99507931+sansej5000@users.noreply.github.com>
2022-05-16 14:24:56 +0300

    Update reverse_string.go

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,13 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	var arr []int32
+	for _, i := range input {
+		arr = append(arr, i)
+	}
+	len := len(arr)
+	for j := len - 1; j >= 0; j-- {
+		output = output + string(arr[j])
+	}
 	return output
 }
